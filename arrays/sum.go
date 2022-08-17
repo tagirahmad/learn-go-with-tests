7d9710dd90a2a2e87e5c661da4555c963e87c680
Tagir <tagirbpl@gmail.com>
2022-08-17 14:50:17 +0300

    learn len(); reflect.DeepEqual; fixed capacity slices and creating new ones from old with append(); benchmarks and refactoring

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,35 @@ func Sum(numbers []int) (sum int) {
 
 	return
 }
+
+func SumAllWithFixedCapacity(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,23 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v is given", got, want)
+	}
+}
+
+func checkSum(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v is given", got, want)
+	}
+}
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -9,9 +26,51 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		want := 6
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
+		checkSum(t, got, want)
+	})
+}
+
+func TestSumAllWithFixedCapacity(t *testing.T) {
+	got := SumAllWithFixedCapacity([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	checkSums(t, got, want)
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	checkSums(t, got, want)
+}
+
+func BenchmarkSumAllWithFixedCapacity(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllWithFixedCapacity([]int{1, 2}, []int{0, 9})
+	}
+}
+
+// Implementation with append and slice of varying size is ~ 2 times slower
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9})
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 
 }
